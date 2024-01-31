ddbe9b3d2fa15e45a834643104fbe91ffa4ff86e
Mohamed Elsayed <mohamed_elsayed.1995@yahoo.com>
2024-01-31 02:24:15 +0200

    Adding Average Service

---
+++ server/AverageServiceImp.cs
@@ -0,0 +1,23 @@
+﻿using Average;
+using Grpc.Core;
+using static Average.averageService;
+
+namespace server;
+
+public class AverageServiceImp : averageServiceBase
+{
+	public override async Task<AverageResponse> avg(IAsyncStreamReader<AverageRequest> requestStream, ServerCallContext context)
+	{
+		int count = 0;
+		double total = 0.0;
+
+		while (await requestStream.MoveNext())
+		{
+			total += requestStream.Current.Number;
+			count++;
+		}
+
+		return new AverageResponse() { Result = total / count };
+	}
+
+}
---
+++ server/PrimeNumberServiceImp.cs
@@ -0,0 +1,27 @@
+﻿using Grpc.Core;
+using Prime;
+using static Prime.PrimeNumberService;
+
+namespace server;
+
+public class PrimeNumberServiceImp : PrimeNumberServiceBase
+{
+	public override async Task PrimeNumberFactor(PrimeNumberRequest request, IServerStreamWriter<PrimeNumberRespose> responseStream, ServerCallContext context)
+	{
+		int number = request.Number;
+		Console.WriteLine($"Server recieved the request for number: {number}");
+		int dividor = 2;
+
+		while (number > 1)
+		{
+			if (number % dividor == 0)
+			{
+				number /= dividor;
+				await responseStream.WriteAsync(new PrimeNumberRespose() { PrimeFactor = dividor });
+			}
+			else
+				dividor++;
+		}
+	}
+
+}
